1ced5c052f69fcf2e071c87ecea380eb9ca18d21
Cristian <cristianhdez23@outlook.com>
2021-03-07 11:53:14 -0500

    Delete Next js Img Implementation and install material-ui-img package

--- pages/cerritos/index.js
+++ pages/cerritos/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
+import Image from "material-ui-image";
 import styles from "../../styles/Home.module.css";
 
 const Cerritos = () => {
@@ -10,14 +10,15 @@ const Cerritos = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Image
-        src="/photos/2020_Blizzconline_Zoom_background_WOW_01.jpeg"
-        alt="GG"
-        layout="fill"
-        objectFit="cover"
-        objectPosition="bottom center"
-        quality={50}
-      />
+      <section className={styles.imageContainer}>
+        <Image
+          src="/photos/2020_Blizzconline_Zoom_background_WOW_01.jpeg"
+          animationDuration={1000}
+          disableSpinner
+          cover
+          style={{paddingTop: '0', height: '100%', width: '100%'}}
+        />
+      </section>
     </main>
   );
 };
