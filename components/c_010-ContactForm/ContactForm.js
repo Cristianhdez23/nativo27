775e9add9bcf44d9237fd6b3560dc34adae17696
Cristian <cristianhdez23@outlook.com>
2021-03-18 00:46:01 -0500

    Trying different setup for form

--- components/c_010-ContactForm/ContactForm.js
+++ components/c_010-ContactForm/ContactForm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { Grid, Typography, TextField, Button } from "@material-ui/core";
 
 import useStyles from "./styles";
@@ -8,7 +8,7 @@ const ContactForm = () => {
   const [success, setSuccess] = useState(false);
 
   useEffect(() => {
-    if ( window.location.search.includes('success=true') ) {
+    if (window.location.search.includes("success=true")) {
       setSuccess(true);
     }
   }, []);
@@ -107,28 +107,26 @@ const ContactForm = () => {
             </Button>
           </form> */}
           {success && (
-          <p style={{ color: 'green'}}>
-            Successfully submitted form!
-          </p>
-        )}
-           <form name="contact" method="POST" action="/?success=true" data-netlify="true">
-              <input type="hidden" name="form-name" value="contact" />
-              <p>
-                <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name" />
-              </p>
-              <p>
-                <label htmlFor="email">Email</label>
-                <input type="text" id="email" name="email" />
-              </p>
-              <p>
-                <label htmlFor="message">Message</label>
-                <textarea id="message" name="message"></textarea>
-              </p>
-              <p>
-                <button type="submit">Send</button>
-              </p>
-            </form>
+            <p style={{ color: "green" }}>Successfully submitted form!</p>
+          )}
+          <form name="contact" method="post" className={classes.FormContacto}>
+            <input type="hidden" name="form-name" value="contact" />
+            <p>
+              <label htmlFor="name">Name</label>
+              <input type="text" id="name" name="name" />
+            </p>
+            <p>
+              <label htmlFor="email">Email</label>
+              <input type="text" id="email" name="email" />
+            </p>
+            <p>
+              <label htmlFor="message">Message</label>
+              <textarea id="message" name="message"></textarea>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
         </Grid>
       </Grid>
     </section>
