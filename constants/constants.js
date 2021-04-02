c4c1cfb7754e137dd3bec8622f6e5dc27761d1d0
Cristian <cristianhdez23@outlook.com>
2021-04-02 17:10:12 -0500

    Adjust LG size

--- constants/constants.js
+++ constants/constants.js
@@ -125,6 +125,15 @@ const theme = createMuiTheme({
       },
     },
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1274,
+      xl: 1920,
+    },
+  },
 });
 
 export default theme;
